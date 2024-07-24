24622888e97c44f4b77268c7e972fd319b2ca044
nikodem-stolarczyk <NikodemStolarczyk319@gmail.com>
2024-07-24 15:27:01 +0200

    Adding simple examples

---
+++ python/simple_usage_example_dispersion.py
@@ -0,0 +1,15 @@
+import mHT 
+
+#example parameters of the S(1) 3-0 line of H2 perturbed by Ar (reference 10.1063/5.0139229)
+nu0 = 112265.5949;# Unperturbed line position in cm-1.
+GamD = 35.1e-3;# Doppler broadening in cm-1
+Gam0 = 11.3e-3;# Speed-averaged line-width in cm-1.
+Gam2 = 0.374e-3;# Speed dependence of the line-width in cm-1.
+Shift0 = -26.4e-3;# Speed-averaged line-shift in cm-1.
+Shift2 = 17.8e-3;# Speed dependence of the line-shift in cm-1.
+NuOptRe = 72.1e-3;# Real part of the Dicke parameter in cm-1.
+NuOptIm = -16.1e-3# Imaginary part of the Dicke parameter in cm-1.
+nu=nu0+1;# Current wavenumber of the computation in cm-1.
+
+print("the output of the mHT function - dispersion")
+print(mHT.profile(nu0,GamD,Gam0,Gam2,Shift0,Shift2,NuOptRe,NuOptIm,nu)[1])
