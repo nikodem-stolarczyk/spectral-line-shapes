db7b20e221edec97917ce348a5005ba45c6f181f
SuoWick <m.slowinski.92@gmail.com>
2024-07-25 13:37:16 +0200

    Update example_absorprtion.py
    Typo correction

--- python/example_absorprtion.py
+++ python/example_absorprtion.py
@@ -11,5 +11,5 @@ NuOptRe = 72.1e-3;# Real part of the Dicke parameter in cm-1.
 NuOptIm = -16.1e-3# Imaginary part of the Dicke parameter in cm-1.
 nu=nu0+1;# Current wavenumber of the computation in cm-1.
 
-print("the output of the mHT function - absorbtion")
+print("the output of the mHT function - absorption")
 print(mHT.profile(nu0,GamD,Gam0,Gam2,Shift0,Shift2,NuOptRe,NuOptIm,nu)[0]) # The mHT function output is a table of its real and imaginary part. The part [0] is its real part, representing the absorption profile.
