63ec8c9bfcf0a8ca717a0fbe75113c335e411a6e
nikodem-stolarczyk <NikodemStolarczyk319@gmail.com>
2024-07-25 12:53:59 +0200

    adding cpf example

---
+++ python/cpf_example.py
@@ -0,0 +1,12 @@
+from mHT.CPF import cpf_fast, cpf_accurate
+
+
+#example parameters for the cpf functions
+x=1; #dimensionless
+y=1; #dimensionless
+
+print("the output of the cpf_accurate function")
+print(cpf_accurate(x,y))
+
+print("the output of the cpf_fast function")
+print(cpf_fast(x,y))
