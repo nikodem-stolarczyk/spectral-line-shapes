287483313fef48ea76990d1e6769046df4a79ff7
SuoWick <m.slowinski.92@gmail.com>
2024-08-08 09:36:26 +0200

    Update example_absorption.m
    Removed Ar subscripts (redundant)

--- matlab/example_absorption.m
+++ matlab/example_absorption.m
@@ -4,14 +4,14 @@ load('mhtconstants.mat')
 
 % Example parameters of the S(1) 3-0 line of H2 perturbed by Ar. 
 % Reference: 10.1063/5.0139229.
-nu0        = 112265.5949; % Unperturbed line position in cm-1.
-GamD       =     35.1e-3; % Doppler broadening in cm-1.
-Gam0_Ar    =     11.3e-3; % Speed-averaged line-width in cm-1.
-Gam2_Ar    =     37.4e-5; % Speed dependence of the line-width in cm-1.
-Shift0_Ar  =    -26.4e-3; % Speed-averaged line-shift in cm-1.
-Shift2_Ar  =     17.8e-3; % Speed dependence of the line-shift in cm-1.
-NuOptRe_Ar =     72.1e-3; % Real part of the Dicke parameter in cm-1.
-NuOptIm_Ar =    -16.1e-3; % Imaginary part of the Dicke parameter in cm-1.
+nu0     = 112265.5949; % Unperturbed line position in cm-1.
+GamD    =     35.1e-3; % Doppler broadening in cm-1.
+Gam0    =     11.3e-3; % Speed-averaged line-width in cm-1.
+Gam2    =     37.4e-5; % Speed dependence of the line-width in cm-1.
+Shift0  =    -26.4e-3; % Speed-averaged line-shift in cm-1.
+Shift2  =     17.8e-3; % Speed dependence of the line-shift in cm-1.
+NuOptRe =     72.1e-3; % Real part of the Dicke parameter in cm-1.
+NuOptIm =    -16.1e-3; % Imaginary part of the Dicke parameter in cm-1.
 
 % Example frequency point:
 nu = nu0 + 1; % Current wavenumber of the computation in cm-1.
