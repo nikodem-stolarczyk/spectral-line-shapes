6b82ca30e6417a79e217ffe48a29c88c277d6663
SuoWick <m.slowinski.92@gmail.com>
2024-10-05 11:35:20 +0200

    Update example_plots.py
    Changes to the example file that reflects recent changes to the profile code.
    
    Added aspect ratio to the plots to make them more horizontal.

--- python/example_plots.py
+++ python/example_plots.py
@@ -4,14 +4,14 @@ from matplotlib import pyplot as plt
 
 #example parameters of the S(1) 3-0 line of H2 perturbed by Ar (reference 10.1063/5.0139229)
 nu0 = 112265.5949;# Unperturbed line position in cm-1.
-GamD = 35.1e-3;# Doppler broadening in cm-1
-Gam0_Ar = 11.3e-3;# Speed-averaged line-width in cm-1.
-Gam2_Ar = 0.374e-3;# Speed dependence of the line-width in cm-1.
-Shift0_Ar = -26.4e-3;# Speed-averaged line-shift in cm-1.
-Shift2_Ar = 17.8e-3;# Speed dependence of the line-shift in cm-1.
+GammaD = 35.1e-3;# Doppler broadening in cm-1
+Gamma0_Ar = 11.3e-3;# Speed-averaged line-width in cm-1.
+Gamma2_Ar = 0.374e-3;# Speed dependence of the line-width in cm-1.
+Delta0_Ar = -26.4e-3;# Speed-averaged line-shift in cm-1.
+Delta2_Ar = 17.8e-3;# Speed dependence of the line-shift in cm-1.
 NuOptRe_Ar = 72.1e-3;# Real part of the Dicke parameter in cm-1.
 NuOptIm_Ar = -16.1e-3# Imaginary part of the Dicke parameter in cm-1.
-nu=nu0+1;# Current wavenumber of the computation in cm-1.
+nu=nu0+1;# Current wavenumber in cm-1.
 
 #optional parameters
 Ylm = 1.0e-3; # Real part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless.
@@ -19,10 +19,10 @@ Xlm = 0.5e-3; # Imaginary part of the 1st order (Rosenkranz) line mixing coeffic
 alpha_Ar = 20; # perturber-to-absorber mass ratio, dimensionless.
 
 #example parameters of the S(1) 3-0 line of H2 perturbed by He (reference 10.1103/PhysRevA.101.052705)
-Gam0_He = 11.7e-3;# Speed-averaged line-width in cm-1.
-Gam2_He = 5.4e-3;# Speed dependence of the line-width in cm-1.
-Shift0_He = 30.5e-3;# Speed-averaged line-shift in cm-1.
-Shift2_He = 12.4e-3;# Speed dependence of the line-shift in cm-1.
+Gamma0_He = 11.7e-3;# Speed-averaged line-width in cm-1.
+Gamma2_He = 5.4e-3;# Speed dependence of the line-width in cm-1.
+Delta0_He = 30.5e-3;# Speed-averaged line-shift in cm-1.
+Delta2_He = 12.4e-3;# Speed dependence of the line-shift in cm-1.
 NuOptRe_He = 38.0e-3;# Real part of the Dicke parameter in cm-1.
 NuOptIm_He = -17.5e-3# Imaginary part of the Dicke parameter in cm-1.
 
@@ -30,9 +30,9 @@ NuOptIm_He = -17.5e-3# Imaginary part of the Dicke parameter in cm-1.
 alpha_He = 2; # perturber-to-absorber mass ratio, dimensionless.
 
 # Preparing tables for plotting 
-x    = nu0 + np.linspace(-5*GamD,+5*GamD,1001)
-y_Ar = [mHT.profile(nu0,GamD,Gam0_Ar,Gam2_Ar,Shift0_Ar,Shift2_Ar,NuOptRe_Ar,NuOptIm_Ar,nu,Ylm,Xlm,alpha_Ar) for nu in x]
-y_He = [mHT.profile(nu0,GamD,Gam0_He,Gam2_He,Shift0_He,Shift2_He,NuOptRe_He,NuOptIm_He,nu,Ylm,Xlm,alpha_He) for nu in x]
+x    = nu0 + np.linspace(-5*GammaD,+5*GammaD,1001)
+y_Ar = [mHT.profile(nu0,GammaD,Gamma0_Ar,Gamma2_Ar,Delta0_Ar,Delta2_Ar,NuOptRe_Ar,NuOptIm_Ar,nu,Ylm=Ylm,Xlm=Xlm,alpha=alpha_Ar,disp=True) for nu in x]
+y_He = [mHT.profile(nu0,GammaD,Gamma0_He,Gamma2_He,Delta0_He,Delta2_He,NuOptRe_He,NuOptIm_He,nu,Ylm=Ylm,Xlm=Xlm,alpha=alpha_He,disp=True) for nu in x]
 
 # Plotting absorption and dispersion of the H2-Ar system 
 plt.plot(x,list(map(list,zip(*y_Ar)))[0])
@@ -41,6 +41,7 @@ plt.grid(True)
 plt.title("Real (blue) and imaginary (orange) part for mHT output for H$_2$-Ar")
 plt.xlabel("Frequency [cm^(-1)]")
 plt.ylabel("mHT")
+plt.gca().set_aspect(0.0075)
 plt.show()
 
 plt.clf()
@@ -51,4 +52,5 @@ plt.grid(True)
 plt.title("Real part of mHT output for H$_2$-Ar (blue) and H$_2$-He (orange).")
 plt.xlabel("Frequency [cm^(-1)]")
 plt.ylabel("mHT")
+plt.gca().set_aspect(0.0075)
 plt.show()
