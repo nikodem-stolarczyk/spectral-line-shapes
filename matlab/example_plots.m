7193ce1abcd05f0ccb4a5c20dabe383e7709ad74
SuoWick <m.slowinski.92@gmail.com>
2024-10-19 12:40:54 +0200

    Update example_plots.m
    Renaming for new comvention, quadratic addition, global reference delete.

--- matlab/example_plots.m
+++ matlab/example_plots.m
@@ -1,27 +1,23 @@
 % Add subfolders to PATH variable
 addpath(genpath(fileparts(which(mfilename))));
 
-% Importing global variables used in subfunctions
-global e pi rp sln2 num0 numinf
-load('mhtconstants.mat')
-
 % Example parameters of the S(1) 3-0 line of H2 perturbed by Ar. 
 % Reference: 10.1063/5.0139229.
 nu0        = 112265.5949; % Unperturbed line position in cm-1.
 GamD       =     35.1e-3; % Doppler broadening in cm-1.
-Gam0_Ar    =     11.3e-3; % Speed-averaged line-width in cm-1.
-Gam2_Ar    =     37.4e-5; % Speed dependence of the line-width in cm-1.
-Shift0_Ar  =    -26.4e-3; % Speed-averaged line-shift in cm-1.
-Shift2_Ar  =     17.8e-3; % Speed dependence of the line-shift in cm-1.
+Gamma0_Ar  =     11.3e-3; % Speed-averaged line-width in cm-1.
+Gamma2_Ar  =     37.4e-5; % Quadratic speed dependence parameter of the line-width in cm-1.
+Delta0_Ar  =    -26.4e-3; % Speed-averaged line-shift in cm-1.
+Delta2_Ar  =     17.8e-3; % Quadratic speed dependence parameter of the line-shift in cm-1.
 NuOptRe_Ar =     72.1e-3; % Real part of the Dicke parameter in cm-1.
 NuOptIm_Ar =    -16.1e-3; % Imaginary part of the Dicke parameter in cm-1.
 
 % Example parameters of the S(1) 3-0 line of H2 perturbed by He.
 % Reference: 10.1103/PhysRevA.101.052705.
-Gam0_He    =     11.7e-3; % Speed-averaged line-width in cm-1.
-Gam2_He    =      5.4e-3; % Speed dependence of the line-width in cm-1.
-Shift0_He  =     30.5e-3; % Speed-averaged line-shift in cm-1.
-Shift2_He  =     12.4e-3; % Speed dependence of the line-shift in cm-1.
+Gamma0_He  =     11.7e-3; % Speed-averaged line-width in cm-1.
+Gamma2_He  =      5.4e-3; % Quadratic speed dependence parameter of the line-width in cm-1.
+Delta0_He  =     30.5e-3; % Speed-averaged line-shift in cm-1.
+Delta2_He  =     12.4e-3; % Quadratic speed dependence parameter of the line-shift in cm-1.
 NuOptRe_He =     38.0e-3; % Real part of the Dicke parameter in cm-1.
 NuOptIm_He =    -17.5e-3; % Imaginary part of the Dicke parameter in cm-1.
 
@@ -32,8 +28,8 @@ yI_Ar = zeros(1,length(x));
 yR_He = zeros(1,length(x));
 yI_He = zeros(1,length(x));
 for i=1:length(x)
-    [yR_Ar(i), yI_Ar(i)] = profile(nu0, GamD, Gam0_Ar, Gam2_Ar, Shift0_Ar, Shift2_Ar, NuOptRe_Ar, NuOptIm_Ar, x(i));
-    [yR_He(i), yI_He(i)] = profile(nu0, GamD, Gam0_He, Gam2_He, Shift0_He, Shift2_He, NuOptRe_He, NuOptIm_He, x(i));
+    [yR_Ar(i), yI_Ar(i)] = profile(nu0, GamD, Gamma0_Ar, Gamma2_Ar, Delta0_Ar, Delta2_Ar, NuOptRe_Ar, NuOptIm_Ar, x(i));
+    [yR_He(i), yI_He(i)] = profile(nu0, GamD, Gamma0_He, Gamma2_He, Delta0_He, Delta2_He, NuOptRe_He, NuOptIm_He, x(i));
 end
 
 % Plotting primary example
