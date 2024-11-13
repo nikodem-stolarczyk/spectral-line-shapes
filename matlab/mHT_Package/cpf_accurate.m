9a00344509c4a3b6cad69640234d0e320830a061
nikodem-stolarczyk <NikodemStolarczyk319@gmail.com>
2024-11-13 13:44:52 +0100

    fixing a typo in a filename

---
+++ matlab/mHT_Package/cpf_accurate.m
@@ -0,0 +1,39 @@
+function w42 = cpf_accurate(x, y)
+    % ---------------------------------------- 
+    %    Computes the complex probability function using a rational series 
+    %    with 42 terms. It is assumed that Im(z) > 0 or Im(z) = 0. (reference: jstor.org/stable/2158232)
+    %    A series was simplified to 37 terms introducing less than 10^(-17) deviations on mHT profile.
+    %
+    %    Standard Input Parameters:
+    %    --------------------
+    %    x   : Real part of input complex parameter
+    %    y   : Imaginary part of input complex parameter
+    %
+    %    The function has one output:
+    %    --------------------
+    %    (1) : Complex probability function
+    % ---------------------------------------- 
+    arguments (Input)
+        x   (1,1) double
+        y   (1,1) double
+    end
+    arguments (Output)
+        w42 (1,1) double
+    end
+
+    rp  = 1.772453850905516; % Root square of pi 
+    L   = 5.449631621480024; % The pre-calculated Weideman constant for N = 42
+    z   = -y + x*1i;
+    Z   = (L + z) / (L - z);
+    a   = [ -3.129493160727961E-14, -1.188364999909099E-14,  1.951777029849348E-13,  1.790586243645278E-13, ...
+            -1.184560208678836E-12, -2.069163661083667E-12,  6.430136110306704E-12,  2.063579921011804E-11, ...
+            -2.392389527320517E-11, -1.799169607159564E-10, -6.353807951660892E-11,  1.282896083944607E-09, ...
+             2.636162411919059E-09, -5.468780625369738E-09, -3.294773119114329E-08, -2.752070035718561E-08, ...
+             2.206733163926054E-07,  8.511689670641750E-07,  4.936972061734341E-07, -6.617492208403963E-06, ...
+            -2.914574364851397E-05, -4.816473680511106E-05,  1.044072210002090E-04,  1.070131083157417E-03, ...
+             4.631075611097791E-03,  1.480296368764821E-02,  3.922970169744468E-02,  9.038744880336540E-02, ...
+             1.857036333535562E-01,  3.455278077566057E-01,  5.882708203344523E-01,  9.230959991941070E-01, ...
+             1.342044484596932E+00,  1.814714451499866E+00,  2.288734169675538E+00,  2.697763665856064E+00, ...
+             2.975931371735470E+00]; % The pre-calculated table of FFT constant terms (truncated from the beginning)
+    w42 = 2*sum(a.*Z.^(36:-1:0))./(L-z).^2 + 1./(L-z)./rp;
+end
---
+++ matlab/mHT_Package/profile.m
@@ -0,0 +1,95 @@
+function [real_part, imag_part] = profile(nu0, GammaD, Gamma0, Gamma2, Delta0, Delta2, NuOptRe, NuOptIm, nu, Ylm, Xlm, alpha)
+    % ---------------------------------------- 
+    %    Subroutine to compute the complex normalized spectral shape of an 
+    %    isolated line by the modified Hartman Tran (mHT) model
+    %
+    %    Standard Input Parameters:
+    %    --------------------
+    %    nu0       : Unperturbed line position in cm-1.
+    %    GammaD    : Doppler HWHM in cm-1.
+    %    Gamma0    : Speed-averaged line-width in cm-1.       
+    %    Gamma2    : Quadratic speed dependence parameter of the line-width in cm-1.
+    %    Delta0    : Speed-averaged line-shift in cm-1.
+    %    Delta2    : Quadratic speed dependence parameter of the line-shift in cm-1.   
+    %    NuOptRe   : Real part of the Dicke parameter in cm-1.
+    %    NuOptIm   : Imaginary part of the Dicke parameter in cm-1.    
+    %    nu        : Current wavenumber in cm-1.
+    %
+    %    Optional Input Parameters:
+    %    --------------------
+    %    Ylm       : Imaginary part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless (default: 0.0).
+    %    Xlm       : Real part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless (default: 0.0).
+    %    alpha     : Mass ratio in the molecule for calculating beta-correction, applicable up to alpha=5, dimensionless (default: 10.0).
+    %
+    %    The function has two outputs:
+    %    --------------------
+    %    real_part : Real part of the normalized spectral shape in cm.
+    %    imag_part : Imaginary part of the normalized spectral shape in cm.
+    % ---------------------------------------- 
+    arguments (Input)
+        nu0       (1,1) double
+        GammaD    (1,1) double
+        Gamma0    (1,1) double
+        Gamma2    (1,1) double
+        Delta0    (1,1) double
+        Delta2    (1,1) double
+        NuOptRe   (1,1) double
+        NuOptIm   (1,1) double
+        nu        (1,1) double
+        Ylm       (1,1) double = 0.0
+        Xlm       (1,1) double = 0.0
+        alpha     (1,1) double = 10.0
+    end    
+    arguments (Output)
+        real_part (1,1) double
+        imag_part (1,1) double
+    end
+    
+    % Define mathematical, numerical and function handles variables
+    pi      = 3.141592653589793;  % Pi number
+    rp      = 1.772453850905516;  % Root square of pi
+    sln2    = 0.8325546111576977; % Root square of natural logarithm of 2
+    num0    = 1.0e-15;            % Numerical zero
+    numinf  = 4000;               % Numerical infinity
+    cpf     = @cpf_accurate;      % CPF choice
+
+    % Ternary operator
+    ternary = @(varargin) varargin{length(varargin)-varargin{1}}; 
+    
+    % The function
+    nuD = GammaD / sln2 ;
+    nuR = NuOptRe*beta(GammaD, NuOptRe, alpha);
+    c2  = Gamma2+Delta2*1i;
+    c0  = Gamma0+Delta0*1i-1.5*c2+nuR+NuOptIm*1i;
+    LM  = 1+Xlm+Ylm*1i;
+    if abs(c2) ~= num0
+        X    = ((nu0-nu)*1i+c0)/c2;
+        Y    = 0.25*(nuD/c2)^2;
+        csqY = 0.50*nuD*(Gamma2-Delta2*1i)/(Gamma2^2+Delta2^2);
+        if abs(Y)>abs(X)*num0
+            z2 = sqrt(X+Y)+csqY;   
+            z1 = ternary(abs(X)>abs(Y)*3e-8,z2-2*csqY, ((nu0-nu)*1i+c0)/nuD);
+            w1 = cpf(-imag(z1), real(z1));
+            w2 = cpf(-imag(z2), real(z2));
+            A  = rp/nuD*(w1-w2);
+        else
+            if abs(sqrt(X)) < numinf
+                rX = sqrt(X);
+                wX = cpf(-imag(rX), real(rX));
+                A  = 2*(1-rp*rX*wX)/c2;
+            else
+                A  = (1/X-1.5/X^2)/c2;
+            end
+        end
+    else
+        z = ((nu0-nu)*1i+c0)/nuD;
+        w = cpf(-imag(z), real(z));
+        A = w*rp/nuD;
+    end
+    I = LM/pi*A/(1-(nuR+NuOptIm*1i)*A);
+    
+    % Output
+    real_part = real(I);
+    imag_part = imag(I);
+end
+
