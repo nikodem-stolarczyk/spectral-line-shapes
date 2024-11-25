4af8beadc79d6a419f561005ce451fac39f28ed4
nikodem-stolarczyk <NikodemStolarczyk319@gmail.com>
2024-11-25 15:32:03 +0100

    Update example_mHT_optional_parameters.f90

--- fortran/example_mHT_optional_parameters.f90
+++ fortran/example_mHT_optional_parameters.f90
@@ -3,15 +3,15 @@ program example_mHT_optional_parameters
    use spectral_module, only: profile
    implicit none
    real(dp) :: absorption, dispersion
-! example parameters of the S(1) 3-0 line of H2 perturbed by Ar (reference 10.1063/5.0139229)
+! Example parameters of the S(1) 3-0 line of H2 perturbed by He (reference: 10.1103/PhysRevA.101.052705)
    real(dp) :: nu0 = 112265.5949_dp ! Unperturbed line position in cm-1.
    real(dp) :: GammaD = 35.1e-3_dp ! Doppler broadening in cm-1.
-   real(dp) :: Gamma0 = 11.3e-3_dp ! Speed-averaged line-width in cm-1.
-   real(dp) :: Gamma2 = 0.374e-3_dp ! Speed dependence of the line-width in cm-1.
-   real(dp) :: Delta0 = -26.4e-3_dp ! Speed-averaged line-Delta in cm-1.
-   real(dp) :: Delta2 = 17.8e-3_dp ! Speed dependence of the line-shift in cm-1.
-   real(dp) :: NuOptRe = 72.1e-3_dp ! Real part of the Dicke parameter in cm-1.
-   real(dp) :: NuOptIm = -16.1e-3_dp ! Imaginary part of the Dicke parameter in cm-1.
+   real(dp) :: Gamma0 = 11.7e-3_dp ! Speed-averaged line-width in cm-1.
+   real(dp) :: Gamma2 = 5.4e-3_dp ! Speed dependence of the line-width in cm-1.
+   real(dp) :: Delta0 = 30.5e-3_dp ! Speed-averaged line-shift in cm-1.
+   real(dp) :: Delta2 = 12.4e-3_dp ! Speed dependence of the line-shift in cm-1.
+   real(dp) :: NuOptRe = 38.0e-3_dp ! Real part of the Dicke parameter in cm-1.
+   real(dp) :: NuOptIm = -17.5e-3_dp ! Imaginary part of the Dicke parameter in cm-1.
    real(dp) :: nu ! Current wavenumber of the computation in cm-1.
    real(dp) :: Ylm ! Real part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless.
    real(dp) :: Xlm ! Imaginary part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless.
