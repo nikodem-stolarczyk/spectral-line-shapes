074ee9919c2c0d54d1267f8b1aa8649bad26fae4
nikodem-stolarczyk <NikodemStolarczyk319@gmail.com>
2025-01-14 13:52:07 +0100

    fixed a bug in example_mHT_optional_parameters.py

--- python/example_mHT_optional_parameters.py
+++ python/example_mHT_optional_parameters.py
@@ -12,16 +12,21 @@ NuOptIm = -17.5e-3    # Imaginary part of the Dicke parameter in cm-1.
 nu      = nu0+1       # Current wavenumber in cm-1.
 
 print("The output of the mHT function:")
-print(mHT.profile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu)) # The mHT function output returns absorption profile by default. To display the dispersive profile see example_dispersion.py.
+print("absorbtion:")
+print(mHT.profile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu))
+print("dispersion:")
+print(mHT.profile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu,disp=True))
 
 # Optional parameters
 newYlm   = 1.0e-3 # Real part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless (default: 0.0).
 newXlm   = 0.5e-3 # Imaginary part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless (default: 0.0).
 newalpha = 2     # Perturber-to-absorber mass ratio, dimensionless (default: 10.0).
-newdisp  = True   # Boolean trigger for including dispersion profile in the output (default: False).
 
 print("The output of the mHT function with optional parameters:")
-print(mHT.profile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu,Ylm=newYlm,Xlm=newXlm,alpha=newalpha,disp=newdisp))
+print("absorbtion:")
+print(mHT.profile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu,Ylm=newYlm,Xlm=newXlm,alpha=newalpha,disp=False))
+print("dispersion:")
+print(mHT.profile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu,Ylm=newYlm,Xlm=newXlm,alpha=newalpha,disp=True))
 # This function accepts 4 optional parameters, provided as dictionary inputs.
 # Parameters can be specified in any order, and it is not necessary to provide all of them.
 # Optional parameters must be passed last in the function call, using their corresponding keyword (e.g., "Ylm=").
