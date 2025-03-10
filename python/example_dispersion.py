c8ec5c54c935ad374c87c4a5f6dff7a5ba2f4952
SuoWick <m.slowinski.92@gmail.com>
2025-03-10 14:36:14 +0100

    Update example_dispersion.py
    Rewritted to match recent changes to mHT profile definition.

--- python/example_dispersion.py
+++ python/example_dispersion.py
@@ -1,6 +1,6 @@
-import mHT 
+from mHT import mHTprofile
 
-# Example parameters of the S(1) 3-0 line of H2 perturbed by He (reference: 10.1103/PhysRevA.101.052705)
+# Example parameters of the S(1) 3-0 line of H2 perturbed by He (Source: 10.1103/PhysRevA.101.052705)
 nu0     = 112265.5949 # Unperturbed line position in cm-1.
 GammaD  = 35.1e-3     # Doppler broadening in cm-1.
 Gamma0  = 11.7e-3     # Speed-averaged line-width in cm-1.
@@ -12,4 +12,4 @@ NuOptIm = -17.5e-3    # Imaginary part of the Dicke parameter in cm-1.
 nu      = nu0+1       # Current wavenumber in cm-1.
 
 print("The output of the mHT function - dispersion:")
-print(mHT.profile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu,disp=True))
+print(mHTprofile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu,0,0,10,True))
