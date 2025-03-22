cb91cf6df68528269e2a97510051abf73f97ab2b
SuoWick <m.slowinski.92@gmail.com>
2025-03-22 12:10:05 +0100

    Update example_absorption.py
    Reference > Source

--- python/example_absorption.py
+++ python/example_absorption.py
@@ -3,7 +3,7 @@ try:
 except ImportError as msg: 
   raise SystemExit (str(msg) + '\nexample_absorption.py: mHT not found. Make sure catalog with mHT module is in same catalog as running script and is named "mHT"!')
 
-# Example parameters of the S(1) 3-0 line of H2 perturbed by He (reference: 10.1103/PhysRevA.101.052705)
+# Example parameters of the S(1) 3-0 line of H2 perturbed by He (Source: 10.1103/PhysRevA.101.052705)
 nu0     = 112265.5949 # Unperturbed line position in cm-1.
 GammaD  = 35.1e-3     # Doppler broadening in cm-1.
 Gamma0  = 11.7e-3     # Speed-averaged line-width in cm-1.
