83d9de087ab88ba6334e44c45edd4657883e42ac
SuoWick <m.slowinski.92@gmail.com>
2025-03-22 12:03:53 +0100

    Update example_mHT_optional_parameters.py
    Fixed mixed Xlm/Ylm comments.
    Reworded comment on output.

--- python/example_mHT_optional_parameters.py
+++ python/example_mHT_optional_parameters.py
@@ -18,13 +18,13 @@ print("The output of the mHT function:")
 print(mHTprofile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu)) # The mHT function output returns absorption profile by default. To display the dispersive profile see example_dispersion.py.
 
 # Optional parameters
-newYlm   = 1.0e-3 # Real part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless (default: 0.0).
-newXlm   = 0.5e-3 # Imaginary part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless (default: 0.0).
+newXlm   = 1.0e-3 # Real part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless (default: 0.0).
+newYlm   = 0.5e-3 # Imaginary part of the 1st order (Rosenkranz) line mixing coefficients, dimensionless (default: 0.0).
 newalpha = 2      # Perturber-to-absorber mass ratio, dimensionless (default: 10.0).
 newdisp  = True   # Boolean trigger for including dispersion profile in the output (default: False).
 
 print("The output of the mHT function with optional parameters:")
 print(mHTprofile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu,newYlm,newXlm,newalpha,newdisp))
 # This function accepts 4 optional parameters.
-# Parameters are not key-worded, so they need to be specified in specific order.
-# To specify Nth parameter all parameters from 0 to N-1 have to be specified as well.
+# Parameters are not key-worded, so they need to be specified in specific order,
+# e.g. to specify dispersion parameter all other parameters has to be specified as well.
