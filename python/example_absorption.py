b90d5ace618f8f6e3d89b480ef4a497fdd525cec
SuoWick <m.slowinski.92@gmail.com>
2025-03-22 11:53:59 +0100

    Update example_absorption.py
    Added try-except structure with suggestion how to include mHT package.

--- python/example_absorption.py
+++ python/example_absorption.py
@@ -1,4 +1,7 @@
-from mHT import mHTprofile
+try: 
+  from mHT import mHTprofile
+except ImportError as msg: 
+  raise SystemExit (str(msg) + '\nexample_absorption.py: mHT not found. Make sure catalog with mHT module is in same catalog as running script and is named "mHT"!')
 
 # Example parameters of the S(1) 3-0 line of H2 perturbed by He (reference: 10.1103/PhysRevA.101.052705)
 nu0     = 112265.5949 # Unperturbed line position in cm-1.
@@ -9,7 +12,7 @@ Delta0  = 30.5e-3     # Speed-averaged line-shift in cm-1.
 Delta2  = 12.4e-3     # Quadratic speed dependence parameter of the line-shift in cm-1.
 NuOptRe = 38.0e-3     # Real part of the Dicke parameter in cm-1.
 NuOptIm = -17.5e-3    # Imaginary part of the Dicke parameter in cm-1.
-nu      = nu0+1       # Current wavenumber in cm-1.
+nu      = nu0+1       # Current wavenumber in cm-1.  
 
 print("The output of the mHT function - absorption:")
 print(mHTprofile(nu0,GammaD,Gamma0,Gamma2,Delta0,Delta2,NuOptRe,NuOptIm,nu)) # The mHT function output returns absorption profile by default.
